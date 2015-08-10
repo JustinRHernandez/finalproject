c8e49c989befc80f06e3baa3353453f3a88edc7d
Justin Hernanez <jjalandoni@inv553.fios-router.home>
2015-08-09 21:19:06 -0400

    Successfully created slideDown function for photography. Also created a design.html file.

--- js/index.js
+++ js/index.js
@@ -3,19 +3,35 @@
   // $(".main__nav").height(h);
   // $(".socialmedia").height(h);
 $(document).ready(function() {
-    function showSecondaryNav(){
+    function showSecondaryNavDesign(){
       event.preventDefault();
   		$("#secondary__nav--design--container").slideDown();
       $("#showSecondaryNav__Design").hide();
       $("#hideSecondaryNav__Design").show();
     }
-    $("#showSecondaryNav__Design").click(showSecondaryNav);
+    $("#showSecondaryNav__Design").click(showSecondaryNavDesign);
 
-    function hideSecondaryNav(){
+    function hideSecondaryNavDesign(){
       event.preventDefault();
   		$("#secondary__nav--design--container").slideUp();
       $("#hideSecondaryNav__Design").hide();
       $("#showSecondaryNav__Design").show();
     }
-    $("#hideSecondaryNav__Design").click(hideSecondaryNav);
+    $("#hideSecondaryNav__Design").click(hideSecondaryNavDesign);
+
+    function showSecondaryNavPhotography(){
+      event.preventDefault();
+      $("#secondary__nav--photography--container").slideDown();
+      $("#showSecondaryNav__Photography").hide();
+      $("#hideSecondaryNav__Photography").show();
+    }
+    $("#showSecondaryNav__Photography").click(showSecondaryNavPhotography);
+
+    function hideSecondaryNavPhotography(){
+      event.preventDefault();
+      $("#secondary__nav--photography--container").slideUp();
+      $("#hideSecondaryNav__Photography").hide();
+      $("#showSecondaryNav__Photography").show();
+    }
+    $("#hideSecondaryNav__Photography").click(hideSecondaryNavPhotography);
 });
