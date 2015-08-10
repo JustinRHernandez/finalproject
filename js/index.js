f392343aab8ac323b45d0acec2ded83e69bbc93f
Justin Hernanez <jjalandoni@inv553.fios-router.home>
2015-08-09 20:52:11 -0400

    Successfully created secondary slideDown menu for design work.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
-$(document).ready(function(){
+
   // var h = $("header").height();
   // $(".main__nav").height(h);
   // $(".socialmedia").height(h);
+$(document).ready(function() {
+    function showSecondaryNav(){
+      event.preventDefault();
+  		$("#secondary__nav--design--container").slideDown();
+      $("#showSecondaryNav__Design").hide();
+      $("#hideSecondaryNav__Design").show();
+    }
+    $("#showSecondaryNav__Design").click(showSecondaryNav);
+
+    function hideSecondaryNav(){
+      event.preventDefault();
+  		$("#secondary__nav--design--container").slideUp();
+      $("#hideSecondaryNav__Design").hide();
+      $("#showSecondaryNav__Design").show();
+    }
+    $("#hideSecondaryNav__Design").click(hideSecondaryNav);
 });
