a412f5e046c85dcba13e6644596758f0e09af4f2
Justin Hernanez <jjalandoni@INV553.local>
2015-08-19 09:05:07 -0400

    -Condensed code -Changed js dropdown to a toggle function -Renamed classes to make more sense with BEM

--- js/index.js
+++ js/index.js
@@ -3,35 +3,44 @@
   // $(".main__nav").height(h);
   // $(".socialmedia").height(h);
 $(document).ready(function() {
-    function showSecondaryNavDesign(){
-      event.preventDefault();
-  		$("#secondary__nav--design--container").slideDown();
-      $("#showSecondaryNav__Design").hide();
-      $("#hideSecondaryNav__Design").show();
-    }
-    $("#showSecondaryNav__Design").click(showSecondaryNavDesign);
+  // var isMenuOpen = false;
+      function toggleMenuDesign(){
+        event.preventDefault();
+    		$("#secondary-nav--design").slideToggle();
+        angle = (angle+90)%360;
+        img.rotateDesign = "rotate"+angle;
+        // $(".rotateDesign").toggleClass("rotate-reset");
+      }
 
-    function hideSecondaryNavDesign(){
-      event.preventDefault();
-  		$("#secondary__nav--design--container").slideUp();
-      $("#hideSecondaryNav__Design").hide();
-      $("#showSecondaryNav__Design").show();
-    }
-    $("#hideSecondaryNav__Design").click(hideSecondaryNavDesign);
+      function toggleMenuPhotography(){
+        event.preventDefault();
+        $("#secondary-nav--photography").slideToggle();
+      }
 
-    function showSecondaryNavPhotography(){
-      event.preventDefault();
-      $("#secondary__nav--photography--container").slideDown();
-      $("#showSecondaryNav__Photography").hide();
-      $("#hideSecondaryNav__Photography").show();
-    }
-    $("#showSecondaryNav__Photography").click(showSecondaryNavPhotography);
+        $("#toggleDesignMenu").click(toggleMenuDesign);
+        $("#togglePhotographyMenu").click(toggleMenuPhotography);
 
-    function hideSecondaryNavPhotography(){
-      event.preventDefault();
-      $("#secondary__nav--photography--container").slideUp();
-      $("#hideSecondaryNav__Photography").hide();
-      $("#showSecondaryNav__Photography").show();
-    }
-    $("#hideSecondaryNav__Photography").click(hideSecondaryNavPhotography);
+    // function hideSecondaryNavDesign(){
+    //   event.preventDefault();
+  	// 	$("#secondary__nav--design--container").slideUp();
+    //   $("#hideSecondaryNav__Design").hide();
+    //   $("#showSecondaryNav__Design").show();
+    // }
+    // $("#hideSecondaryNav__Design").click(hideSecondaryNavDesign);
+    //
+    // function showSecondaryNavPhotography(){
+    //   event.preventDefault();
+    //   $("#secondary__nav--photography--container").slideDown();
+    //   $("#showSecondaryNav__Photography").hide();
+    //   $("#hideSecondaryNav__Photography").show();
+    // }
+    // $("#showSecondaryNav__Photography").click(showSecondaryNavPhotography);
+    //
+    // function hideSecondaryNavPhotography(){
+    //   event.preventDefault();
+    //   $("#secondary__nav--photography--container").slideUp();
+    //   $("#hideSecondaryNav__Photography").hide();
+    //   $("#showSecondaryNav__Photography").show();
+    // }
+    // $("#hideSecondaryNav__Photography").click(hideSecondaryNavPhotography);
 });
