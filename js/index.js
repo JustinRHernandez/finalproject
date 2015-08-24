db350c7d47fb65cece90a73551c4fab74af5570a
Justin Hernanez <jjalandoni@inv553.fios-router.home>
2015-08-23 22:12:25 -0400

    Removed Royal Slider Gallery. About to restructure navigation and move it to the side of the page because images for photography gallery are being cropped with the current layout.

--- js/index.js
+++ js/index.js
@@ -1,35 +1,21 @@
-$(document).ready(function() {
-  var designMenuOpen = false;
-  var photoMenuOpen = false;
-      function toggleMenuDesign(){
-        event.preventDefault();
-        var designMenuOpen = true;
-    		$("#secondary-nav--design").slideToggle();
-        if (photoMenuOpen = true) {
-          $("#secondary-nav--design").show();
-          $("#secondary-nav--photography").hide();
-        }
-      }
-
-      function toggleMenuPhotography(){
-        event.preventDefault();
-        var photoMenuOpen = true;
-        $("#secondary-nav--photography").slideToggle();
-        if (designMenuOpen = true) {
-          $("#secondary-nav--photography").show();
-          $("#secondary-nav--design").hide();
-        }
-      }
-
-        $("#toggleDesignMenu").click(toggleMenuDesign);
-        $("#togglePhotographyMenu").click(toggleMenuPhotography);
-
-        $(".royalSlider").royalSlider({
-          autoScaleSlider: true,
-          loop: true,
-          keyboardNavEnabled: true
-                });
-
-
-
-});
+// $(document).ready(function() {
+//   var designMenuOpen = false;
+//   var photoMenuOpen = false;
+//       function toggleMenuDesign(){
+//         event.preventDefault();
+//         var designMenuOpen = true;
+//     		$("#secondary-nav--design").slideToggle();
+//         if (photoMenuOpen = true) {
+//           $("#secondary-nav--design").show();
+//           $("#secondary-nav--photography").hide();
+//         }
+//       }
+//
+//         $("#toggleDesignMenu").click(toggleMenuDesign);
+//         $("#togglePhotographyMenu").click(toggleMenuPhotography);
+//
+//
+//
+//
+//
+// });
