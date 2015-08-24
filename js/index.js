afbf04d3e72bd60df34f8f4d189a9bc25d723174
Justin Hernanez <jjalandoni@inv553.fios-router.home>
2015-08-23 22:59:49 -0400

    Restructured Nav. Image Gallery now on right side

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
-// $(document).ready(function() {
-//   var designMenuOpen = false;
-//   var photoMenuOpen = false;
-//       function toggleMenuDesign(){
-//         event.preventDefault();
-//         var designMenuOpen = true;
-//     		$("#secondary-nav--design").slideToggle();
-//         if (photoMenuOpen = true) {
-//           $("#secondary-nav--design").show();
-//           $("#secondary-nav--photography").hide();
-//         }
-//       }
-//
-//         $("#toggleDesignMenu").click(toggleMenuDesign);
-//         $("#togglePhotographyMenu").click(toggleMenuPhotography);
-//
-//
-//
-//
-//
-// });
+$(document).ready(function() {
+  var designMenuOpen = false;
+  var photoMenuOpen = false;
+      function toggleMenuDesign(){
+        event.preventDefault();
+        var designMenuOpen = true;
+    		$("#secondary-nav--design").slideToggle();
+        if (photoMenuOpen = true) {
+          $("#secondary-nav--design").show();
+          $("#secondary-nav--photography").hide();
+        }
+      }
+
+        $("#toggleDesignMenu").click(toggleMenuDesign);
+        $("#togglePhotographyMenu").click(toggleMenuPhotography);
+
+
+
+
+
+});
