98c5eb55145e38f33f01da5bd9f74d72f0ed4eed
Justin Hernanez <jjalandoni@inv553.fios-router.home>
2015-08-29 10:00:34 -0400

    Hid nav on mobile.

--- js/index.js
+++ js/index.js
@@ -5,19 +5,19 @@ $(document).ready(function() {
       function toggleMenuDesign(){
         event.preventDefault();
         var designMenuOpen = true;
-    		$("#secondary-nav--design").slideToggle();
-        $(".rotateDesign").rotate(180);
+    		$("#secondary-nav--design").slideToggle(450);
       }
 
       function toggleMenuPhotography(){
         event.preventDefault();
         var photoMenuOpen = true;
-        $("#secondary-nav--photography").slideToggle();
+        $("#secondary-nav--photography").slideToggle(450);
       }
 
 
-        $("#toggleDesignMenu").click(toggleMenuDesign);
+        $("#toggleDesignMenu,").click(toggleMenuDesign);
         $("#togglePhotographyMenu").click(toggleMenuPhotography);
+        $("#toggleMenu-Mobile").click(toggleMenuMobile);
         $(".royalSlider").royalSlider({
           autoScaleSlider: true,
           autoScaleSliderHeight: 475,
