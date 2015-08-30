81f0de3be775a3306832d573b35651f411fa758c
Justin Hernanez <jjalandoni@inv553.fios-router.home>
2015-08-29 20:35:44 -0400

    Animated menu arrows.

--- js/index.js
+++ js/index.js
@@ -2,25 +2,40 @@ $(document).ready(function() {
   var designMenuOpen = false;
   var photoMenuOpen = false;
   var mobileMenuOpen = false;
+  var rotationDesign = 0;
+  var rotationPhoto = 0;
 
       function toggleMenuDesign(){
         event.preventDefault();
         var designMenuOpen = true;
-    		$("#secondary-nav--design").slideToggle(450);
+    		$("#secondary-nav--design").slideToggle();
       }
 
       function toggleMenuPhotography(){
         event.preventDefault();
         var photoMenuOpen = true;
-        $("#secondary-nav--photography").slideToggle(450);
+        $("#secondary-nav--photography").slideToggle();
       }
 
       function toggleMenuMobile(){
         event.preventDefault();
         var mobileMenuOpen = true;
-        $(".primarynav").slideToggle(450);
+        $(".primarynav").slideToggle();
       }
 
+      $(window).on('resize', function(){
+        var win = $(this); //this = window
+        if (win.width() > 768) {
+          $(".primarynav").css("display","block")
+          }
+      });
+
+      $(window).on('resize', function(){
+        var win = $(this); //this = window
+        if (win.width() < 768) {
+          $(".primarynav").css("display","none");
+        }
+      });
 
 
         $("#toggleDesignMenu").click(toggleMenuDesign);
@@ -32,7 +47,29 @@ $(document).ready(function() {
           loop: true,
           transitionType: "fade",
           transitionSpeed: 400,
-
           keyboardNavEnabled: true
         });
+        $("#toggleDesignMenu").click(function(){
+            rotationDesign += 180;
+            $(this).find(".arrowDesign").rotate(rotationDesign);
+            });
+            jQuery.fn.rotate = function(degrees) {
+            $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
+                  '-moz-transform' : 'rotate('+ degrees +'deg)',
+                  '-ms-transform' : 'rotate('+ degrees +'deg)',
+                  'transform' : 'rotate('+ degrees +'deg)'});
+            return $(this);
+            };
+            $("#togglePhotographyMenu").click(function(){
+                rotationPhoto += 180;
+                $(this).find(".arrowPhoto").rotate(rotationPhoto);
+                });
+                jQuery.fn.rotate = function(degrees) {
+                $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
+                      '-moz-transform' : 'rotate('+ degrees +'deg)',
+                      '-ms-transform' : 'rotate('+ degrees +'deg)',
+                      'transform' : 'rotate('+ degrees +'deg)'});
+                return $(this);
+                };
+
 });
